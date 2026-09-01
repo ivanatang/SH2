2e22f386b47aee48d915af721a718dabd74d311c
Ivana Tang <ivana.tang@colorado.edu>
2026-09-01 14:44:23 -0600

    Add output files from WT SH2 with pYEEI test and validation runs

---
+++ boltz2/WT_SH2_pYEEI/test_boltz.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#SBATCH --partition=aa100
+#SBATCH --gres=gpu:a100_3g.20gb:1
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --mem=32G
+#SBATCH --qos=gpu-testing
+#SBATCH --time=00:15:00
+#SBATCH --job-name=boltz_test
+#SBATCH --output=boltz_test.%j.out
+
+module purge
+module load anaconda
+conda activate boltz2
+
+boltz predict /projects/$USER/SH2/boltz2/WT_SH2_pYEEI/cSrc_SH2_EPQpYEEIPIYL.yaml \
+    --out_dir /projects/$USER/SH2/boltz2/WT_SH2_pYEEI/boltz_test \
+    --use_msa_server \
+    --use_potentials \
+    --diffusion_samples 1 \
+    --recycling_steps 3 \
+    --seed 1 \
+    --no_kernels
