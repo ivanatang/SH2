91766f38523bc9ed174a0912cf119115e2a970b5
Ivana Tang <ivana.tang@colorado.edu>
2026-09-01 14:10:10 -0600

    Move test run output files to explicit test_run/ directory

--- af3/test_run/run_af3_sh2.sh
+++ af3/test_run/run_af3_sh2.sh
@@ -12,9 +12,9 @@
 module purge
 module load alphafold/3.0.0
 
-export INPUT_FILE=$(pwd)/superbinder_sh2_pTyr.json
-export OUTPUT_DIR=$(pwd)/output
-export AF3_MODEL_PARAMETERS_DIR=CHANGE_ME   # path where you stored the downloaded AF3 weights
+export INPUT_FILE=$(pwd)/test_run/superbinder_sh2_pTyr.json
+export OUTPUT_DIR=$(pwd)/test_run/output
+export AF3_MODEL_PARAMETERS_DIR=/projects/ivta1597/alphafold3_weights/   # path where you stored the downloaded AF3 weights
 
 mkdir -p "$OUTPUT_DIR"
 
